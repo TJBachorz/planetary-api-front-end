4f2b1bc5ee88c8a4558a471e33229ddb8df7ce5e
boyloe <boyloe@gmail.com>
2020-09-01 14:43:57 -0600

    Add Planet images and some styling

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function makePlanetCard(planets) {
     planets.forEach(planet => {
         planetCard = makeCardSection()
         planetName(planet, planetCard)
+        displayPlanetImage(planet, planetCard)
         planetMoons(planet, planetCard)
         planetGravity(planet, planetCard)
         planetDiameter(planet, planetCard)
@@ -24,27 +25,37 @@ function makeCardSection() {
 
 
 function planetName(planet, planetCard) {
-    const name = document.createElement('h3')
-    name.textContent = `${planet.name}`
+    const name = document.createElement('h2')
+    name.textContent = planet.name
+    
+    // name.innerHTML = `<a href='./img/svg/${planet.name}.svg'>${planet.name}</a>`
     planetCard.appendChild(name)
 }
 
 function planetMoons(planet, planetCard) {
     const moons = document.createElement('p')
-    moons.textContent = planet.moons
+    moons.innerHTML = `Number of Moons: <span class='result'><strong>${planet.moons}</strong></span>`
     planetCard.appendChild(moons)
 }
 
 function planetGravity(planet, planetCard) {
     const gravity = document.createElement('p')
-    gravity.textContent = planet.gravity
+    gravity.innerHTML = `Gravity relative to Earth: <span class='result'><strong>${planet.gravity}</strong></span>`
     planetCard.appendChild(gravity)
 }
 
 function planetDiameter(planet, planetCard) {
     const diameter = document.createElement('p')
-    diameter.textContent = planet.diameter
+    diameter.innerHTML = `Diameter: <span class='result'><strong>${planet.diameter}</strong></span>`
     planetCard.appendChild(diameter)
+}
+
+function displayPlanetImage(planet, planetCard) {
+    const image = document.createElement('img')
+    image.classList.add('planet-image')
+    image.src = `./img/svg/${planet.name}.svg`
+    image.innerHTML = `<a href='./img/svg/${planet.name}.svg'></a>`
+    planetCard.appendChild(image)
 }
 
 
