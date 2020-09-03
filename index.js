19a40c120eb0e96e4862330af7a969f47ea3b2e7
TJBachorz <TJBachorz@gmail.com>
2020-09-03 16:59:24 -0600

    stops planets, needs refinement

--- index.js
+++ index.js
@@ -2,47 +2,79 @@ fetch("http://localhost:3000/planets")
     .then(response => response.json())
     .then(planets => makePlanetCard(planets))
 
-
 function makePlanetCard(planets) {
-    planetCard = makeCardSection()
     planets.forEach(planet => {
+        planetCard = makeCardSection()
         planetName(planet, planetCard)
+        displayPlanetImage(planet, planetCard)
         planetMoons(planet, planetCard)
         planetGravity(planet, planetCard)
         planetDiameter(planet, planetCard)
+        planetDistance(planet,planetCard) 
     })
 }
 
 function makeCardSection() {
-    const body = document.querySelector('body')
-    const planetCard = document.createElement('section')
+    const body = document.querySelector('.test-card')
+    const planetCard = document.createElement('p')
     planetCard.classList.add('planet-card')
     body.appendChild(planetCard)
     return planetCard
 }
 
 function planetName(planet, planetCard) {
-    const name = document.createElement('h3')
+    const name = document.createElement('h2')
     name.textContent = planet.name
     planetCard.appendChild(name)
 }
 
 function planetMoons(planet, planetCard) {
     const moons = document.createElement('p')
-    moons.textContent = planet.moons
+    moons.innerHTML = `Number of Moons: <span class='result'><strong>${planet.moons}</strong></span>`
     planetCard.appendChild(moons)
 }
 
 function planetGravity(planet, planetCard) {
     const gravity = document.createElement('p')
-    gravity.textContent = planet.gravity
+    gravity.innerHTML = `Gravity relative to Earth: <span class='result'><strong>${planet.gravity}</strong></span>`
     planetCard.appendChild(gravity)
 }
 
 function planetDiameter(planet, planetCard) {
     const diameter = document.createElement('p')
-    diameter.textContent = planet.diameter
+    diameter.innerHTML = `Diameter: <span class='result'><strong>${planet.diameter} km</strong></span>`
     planetCard.appendChild(diameter)
 }
 
+function displayPlanetImage(planet, planetCard) {
+    const image = document.createElement('img')
+    image.classList.add('planet-image')
+    image.src = `./img/svg/${planet.name}.svg`
+    image.innerHTML = `<a href='./img/svg/${planet.name}.svg'></a>`
+    planetCard.appendChild(image)
+}
 
+function planetDistance(planet, planetCard) {
+    const distance = document.createElement('p')
+    distance.innerHTML = `Distance from the Sun: <span class='result'><strong>${planet.perihelion} km</strong></span>`
+    planetCard.appendChild(distance)
+}
+
+// -----------------------------------------------------------
+
+const allPlanets = document.querySelectorAll('.center > .smallPlanet')
+console.log(allPlanets)
+allPlanets.forEach(planet => {
+    planet.addEventListener('mouseover', stopPlanet(planet))
+    })
+
+const header = document.querySelector("h1")
+header.addEventListener('mouseover', changeHeader)
+
+function changeHeader() {
+    return header.style.background === 'green' ? header.style.background = 'gray' : header.style.background = 'green'
+}
+
+function stopPlanet(planet) {
+    planet.style.animation = 'none';
+}
